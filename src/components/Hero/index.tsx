fc7fbb4cbf4087aaaeb4035262338df8bb00743c
Richard Freitas <richardfelipelives@gmail.com>
2023-12-27 20:21:41 -0300

    correção de erros

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,16 +1,14 @@
-import { Container } from "../../global"
-import { DivContainer, Formulario, HeroTitle } from "./styles"
-
-
+import { Container } from '../../global'
+import { DivContainer, Formulario, HeroTitle } from './styles'
 
 const Hero = () => (
   <Formulario>
     <Container>
-    <DivContainer>
-      <HeroTitle>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </HeroTitle>
-    </DivContainer>
+      <DivContainer>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </DivContainer>
     </Container>
   </Formulario>
 )
